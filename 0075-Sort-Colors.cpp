a679351297441c148abd5a3a9f23aa37a9eca4c8
zhaomengmeng <zhaomzhao@hotmail.com>
2019-11-28 22:46:45 +0800

    补充

--- 0075-Sort-Colors.cpp
+++ 0075-Sort-Colors.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-
+        // 方法1：冒泡排序
         if(nums.size() == 0)
             return;
 
@@ -15,5 +15,44 @@ public:
                 }
             }
         }
+    }
+};
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        // 方法2：使用三个变量，分别记录红色、白色和蓝色的位置，类似于双指针法
+        if(nums.size() == 0)
+            return;
+
+        int zero_pos = 0;
+        int two_pos = nums.size() - 1;
+
+        for(auto i = 0; i <= two_pos;)
+        {
+            if(nums[i] == 0)
+            {
+                swap(nums[zero_pos], nums[i]);
+                zero_pos++;
+                i++;
+            }
+            else if( nums[i] == 2)
+            {
+                swap(nums[two_pos], nums[i]);
+                two_pos--;
+            }
+            else
+            {
+                i++;
+            }
+        }
+    }
+
+    void swap(int & lhs, int &rhs)
+    {
+        if(lhs == rhs)
+            return;
+        
+        std::swap(lhs, rhs);
     }
 };
