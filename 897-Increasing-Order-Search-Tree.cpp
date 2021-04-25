2778d2e2f7517be32784d603aa2878c49f6c81ff
zhaomengmeng <zhaomzhao@hotmail.com>
2021-04-26 00:42:26 +0800

    non recursive

--- 897-Increasing-Order-Search-Tree.cpp
+++ 897-Increasing-Order-Search-Tree.cpp
@@ -6,29 +6,52 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    TreeNode * pre;
-    TreeNode* increasingBST(TreeNode* root) {
+    TreeNode *pre;
+    TreeNode *increasingBST(TreeNode *root) {
         pre = new TreeNode(-1);
-        TreeNode * p = pre;
+        TreeNode *p = pre;
         inorderTranversal(root);
-        TreeNode * res = p->right;
+        TreeNode *res = p->right;
         delete p;
         return res;
     }
 
-    void inorderTranversal(TreeNode * root) {
-        if(root == nullptr) {
-            return; 
+    void inorderTranversal(TreeNode *root) {
+        if (root == nullptr) {
+            return;
         }
         inorderTranversal(root->left);
         root->left = nullptr;
         pre->right = root;
         pre = root;
         inorderTranversal(root->right);
+    }
+
+    TreeNode *increasingBSTNonRecursive(TreeNode *root) {
+        TreeNode *dummy = new TreeNode(-1);
+        TreeNode *res = dummy;
+
+        stack<TreeNode *> st;
+        TreeNode *p = root;
+        while (p || !st.empty()) {
+            if (p) {
+                st.push(p);
+                p = p->left;
+            } else {
+                auto top = st.top();
+                st.pop();
+                p = top->right;
+                top->left = nullptr;
+                dummy->right = top;
+                dummy = top;
+            }
+        }
+        return res->right;
     }
 };
