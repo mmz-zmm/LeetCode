dcab9b4131e0bfeee52e4482d0ef1ab410e565ab
zhaomengmeng <zhaomzhao@hotmail.com>
2021-05-31 21:59:46 +0800

    bit op with a mask

--- 342-Power-of-Four.cpp
+++ 342-Power-of-Four.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // 开方后可以归为2的幂
     bool isPowerOfFour(int n) {
         if (n <= 0) {
             return false;
@@ -11,5 +12,10 @@ public:
         } else {
             return !(square_root & (square_root - 1));
         }
+    }
+    // 构造一个mask,偶数位位1，即 10101010101010101010101010101010
+    // 表示为16进制为 AAAAAAAA
+    bool isPowerOfFour2(int n) {
+        return n > 0 && !(n & (n - 1)) && !(n & 0xAAAAAAAA);
     }
 };
