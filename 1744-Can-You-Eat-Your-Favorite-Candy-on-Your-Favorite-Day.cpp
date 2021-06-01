b7ec206e91701ceb3ef2d1bddf25aefd4ff79081
zhaomengmeng <zhaomzhao@hotmail.com>
2021-06-02 00:13:11 +0800

    math

---
+++ 1744-Can-You-Eat-Your-Favorite-Candy-on-Your-Favorite-Day.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<bool> canEat(vector<int>& candiesCount, vector<vector<int>>& queries) {
+        int n = candiesCount.size();
+        vector<long long> candiesPrefixSum(n + 1);
+
+        for(int i = 1; i <= n; ++i) {
+            candiesPrefixSum[i] = candiesPrefixSum[i - 1] + candiesCount[i - 1];
+        }
+
+        vector<bool> res;
+        res.reserve(queries.size());
+
+        int type, day, cap;
+        long long earliest, latest;
+        for(auto & query : queries) {
+            type = query[0];
+            day = query[1] + 1;
+            cap = query[2];
+            earliest = (candiesPrefixSum[type]) / cap + 1;
+            latest = candiesPrefixSum[type + 1];
+
+            if(day >= earliest && day <= latest) {
+                res.push_back(true);
+            } else {
+                res.push_back(false);
+            }
+        }
+        return res;
+        
+    }
+};
