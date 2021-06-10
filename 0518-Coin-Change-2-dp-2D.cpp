a566e3f070e8b2d0f40d6ddbf2251658b37b6de1
zhaomengmeng <zhaomzhao@hotmail.com>
2021-06-10 23:57:55 +0800

    dp

--- 0518-Coin-Change-2-dp-2D.cpp
+++ 0518-Coin-Change-2-dp-2D.cpp
@@ -5,25 +5,45 @@ public:
     1. 不放： dp[i][j] = dp[i-1][j];
     2. 放  : dp[i][j] = dp[i][j - amount[i]]
     */
-    int change(int amount, vector<int>& coins) {
+    int change(int amount, vector<int> &coins) {
         int sz = coins.size();
-        if( amount == 0) return 1;
+        if (amount == 0)
+            return 1;
 
         vector<vector<int>> dp(sz + 1, vector<int>(amount + 1, 0));
         // amount为0,那就只有一种组合，就是没有组合。
-        for(int i = 0; i <= sz; ++i) {
+        for (int i = 0; i <= sz; ++i) {
             dp[i][0] = 1;
         }
 
         for (int i = 1; i <= sz; ++i) {
             for (int j = 1; j <= amount; ++j) {
-                if( (j - coins[i-1]) >= 0) {
-                    dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]];
+                if ((j - coins[i - 1]) >= 0) {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
                 } else {
-                    dp[i][j] = dp[i-1][j];
+                    dp[i][j] = dp[i - 1][j];
                 }
             }
-        } 
+        }
         return dp[sz][amount];
+    }
+
+    //  这是最原始的解法，思考，为什么 最内层k的循环(用于表示有k个硬币)可以被替代为 dp[i - 1][j - coins[i - 1]]？
+    // 实际上是因为j是从小到大逐渐递减的，
+    int change2(int amount, vector<int> &coins) {
+        int n = coins.size();
+        vector<vector<int>> dp(n + 1, vector<int>(amount + 1));
+        dp[0][0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j <= amount; ++j) {
+                dp[i][j]  = dp[i - 1][j];
+                int k = j;
+                while (k >= coins[i - 1]) {
+                    k -= coins[i - 1];
+                    dp[i][j] += dp[i - 1][k];
+                }
+            }
+        }
+        return dp[n][amount];
     }
 };
