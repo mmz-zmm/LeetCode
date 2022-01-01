d1235ed121cacde944579816aaa49125ce98ac96
zhaomengmeng <zhaomzhao@hotmail.com>
2022-01-01 23:27:21 +0800

    array

---
+++ 2022-Convert-1D-Array-Into-2D-Array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int> &original, int m, int n) {
+        int size = original.size();
+        if (m * n != size) {
+            return {};
+        }
+        vector<vector<int>> res(m, vector<int>(n));
+
+        for (int i = 0; i < size; ++i) {
+            res[i / n][i % n] = original[i];
+        }
+
+        return res;
+    }
+};
